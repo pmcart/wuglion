21d1c66c8e34f1348bf8b51e0a5098f0a378ecd0
Patrick McArt <patrickmcart@gmail.com>
2018-10-31 22:28:29 +0000

    DB working

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-//import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import {
   FacebookService,
   LoginResponse
 } from 'ngx-facebook';
-
 import Amplify, { Auth } from 'aws-amplify';
 import { environment } from './../../environments/environment';
 import {
@@ -23,37 +21,36 @@ import {
 })
 export class SignupPage implements OnInit {
 
-  constructor(private amplify: Amplify, private fb: FacebookService, private http:Http) { 
+  constructor(private amplify: Amplify, private fb: FacebookService, private http: Http) {
     Amplify.configure(environment.amplify);
   }
 
   ngOnInit() {
   }
 
-  signup(username:string,password:string){
+  signup(username: string, password: string) {
+    // Cognito Signup
     Auth.signUp({
       username,
       password
       })
-      .then(data => 
-        {
-          console.log(data)
-        
-          var options = new RequestOptions({
+      .then(data => {
+          console.log(data);
+          const options = new RequestOptions({
             headers : new Headers({
-              'Content-Type':'application/json' 
+              'Content-Type': 'application/json'
             })
           });
-         
-            this.http.post(environment.api +'/users/create', data,options).subscribe(
-              data => {
-                console.log(data)
-              },
-              error => {
-                console.log(JSON.stringify(error.json()));
-              }
-            )
-          
+          // Create entry in DB with new user
+          this.http.post(environment.api + '/users/create', data, options).subscribe(
+            data2 => {
+              console.log(data2);
+            },
+            error => {
+              console.log(JSON.stringify(error.json()));
+            }
+          );
+
         })
       .catch(err => console.log(err));
   }
@@ -63,23 +60,23 @@ export class SignupPage implements OnInit {
 
       this.fb.login().then((response: LoginResponse) => {
         if (response.authResponse) {
-          const token = response.authResponse.accessToken
-          const expires = response.authResponse.expiresIn
-          const userID = response.authResponse.userID
-          console.log(response.authResponse)
-          console.log('You are now logged in.')
-        
+          const token = response.authResponse.accessToken;
+          const expires = response.authResponse.expiresIn;
+          const userID = response.authResponse.userID;
+          console.log(response.authResponse);
+          console.log('You are now logged in.');
+
           Auth.federatedSignIn('facebook', { token, expires_at: expires}, { name: 'test' })
           .then(credentials => {
             console.log('get aws credentials', credentials);
           }).catch(e => {
             console.log(e);
           });
-     
+
         } else {
           console.log('There was a problem logging you in.');
         }
-      
+
         }).catch((error: any) => console.error(error));
       }
 
