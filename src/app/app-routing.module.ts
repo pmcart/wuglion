f62d0977ee0b47ca5fa3f9db7ecb7069fa5224d8
Patrick McArt <patrickmcart@gmail.com>
2018-11-19 20:24:48 +0000

    Adding main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'main', loadChildren: './main/main.module#MainPageModule' },
 ];
 
 @NgModule({
